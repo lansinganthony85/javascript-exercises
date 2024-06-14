4a89c9f59efb07c7be7231b8e052166f741e12d4
lansinganthony85 <34491888+lansinganthony85@users.noreply.github.com>
2024-06-14 10:15:21 -0400

    solution for 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    splicedArray = arr;
+
+    for(const value of args) {
+
+        while(splicedArray.indexOf(value) >= 0)
+            splicedArray = splicedArray.toSpliced(splicedArray.indexOf(value), 1);
+        
+    }
+
+    return splicedArray;
 };
 
 // Do not edit below this line
