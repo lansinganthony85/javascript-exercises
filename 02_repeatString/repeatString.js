01ab681a17b489633b8fa4218062be5269cee556
lansinganthony85 <34491888+lansinganthony85@users.noreply.github.com>
2024-06-14 09:50:00 -0400

    Solutions to 01, 02, 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(textToRepeat, occurence) {
 
+    if(occurence < 0)
+        return 'ERROR';
+    
+    repeatedText = "";
+    for(let i = 0; i < occurence; i++) {
+        repeatedText += textToRepeat;
+    }
+
+    return repeatedText;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(textToReverse) {
+    reversedText = [];
 
+    for(const letter of textToReverse.split("")) {
+        reversedText.unshift(letter);
+    }
+
+    return reversedText.join("");
 };
 
 // Do not edit below this line
