ccdac390cea9128ba773870a900b42d301ab0fb1
lansinganthony85 <34491888+lansinganthony85@users.noreply.github.com>
2024-06-14 10:29:00 -0400

    solution for 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
 
+if(!(typeof first === "number") || !(typeof second === "number"))
+    return 'ERROR';
+else if(first < 0 || second < 0)
+    return 'ERROR';
+
+return ((Math.max(first, second) - Math.min(first, second) + 1)/2) * (first + second);
 };
 
 // Do not edit below this line
