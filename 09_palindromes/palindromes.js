d787a3f5d7da504f66dc1bdea0d4f45e3858fd31
Anthony Lansing <lansinganthony85@gmail.com>
2024-06-25 11:00:52 -0400

    solution to 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let palindrome = '';
+    let text = '';
 
+    for(let i = 0; i < string.length; i++) {
+        if (string.charAt(i).toUpperCase(0) != string.charAt(i).toLowerCase() || !isNaN(parseInt(string.charAt(i))))
+            text += string.charAt(i).toLowerCase();
+    }
+
+    for(let i = text.length - 1; i >= 0; i--) {
+        palindrome += text.charAt(i);
+    }
+
+    return palindrome === text;
 };
 
 // Do not edit below this line
