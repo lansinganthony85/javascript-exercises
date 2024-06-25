a6cbe854a7a01bc90c523012cbd4dde423b69521
Anthony Lansing <lansinganthony85@gmail.com>
2024-06-25 09:07:39 -0400

    solution for 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	if(arr.length == 0)
+    return 0;
+
+  return arr.reduce((acc, curr) => acc + curr);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, curr) => acc * curr);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let product = 1;
+  for(let i = 0; i < b; i++)
+    product *= a;
+
+  return product;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num == 0 || num == 1)
+    return 1;
+
+  let factorial = 1;
+  for(let i = 1; i <= num; i++)
+    factorial *= i;
+
+  return factorial;
 };
 
 // Do not edit below this line
