68b8fe8578f3ae8e6397c1e82a11e9bcb039a77a
Anthony Lansing <lansinganthony85@gmail.com>
2024-06-25 11:23:15 -0400

    solution for 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    if(typeof num === 'string')
+        value = parseInt(num);
+    else
+        value = num;
+
+    if(value < 0)
+        return "OOPS";
+
+    if(value == 1)
+        return 1;
+
+    let fibonacci = 0;
+    let first = 0;
+    let sec = 1;
+
+    for(let i = 1; i < value; i++) {
+        fibonacci = first + sec;
+        first = sec;
+        sec = fibonacci;
+    }
+
+    return fibonacci;
 };
 
 // Do not edit below this line
