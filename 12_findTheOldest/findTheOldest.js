41a190d5d82d906cfd82866f032accd649ca1323
Anthony Lansing <lansinganthony85@gmail.com>
2024-06-25 20:37:23 -0400

    solution for 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    return array.reduce((acc, curr) => {
+        const date = new Date();
+        const prevDeath = (!("yearOfDeath" in acc)) ? date.getFullYear() : acc.yearOfDeath;
+        const currDeath = (!("yearOfDeath" in curr)) ? date.getFullYear() : curr.yearOfDeath;
+        const prevAge = prevDeath - acc.yearOfBirth;
+        const currAge = currDeath - curr.yearOfBirth;
 
+        if(prevAge > currAge)
+            return acc;
+        else
+            return curr;
+    });
 };
 
 // Do not edit below this line
